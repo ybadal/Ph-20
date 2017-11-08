17eb13e7a295cc831f521f71e76955037a240793
Yovan Badal <yovanbadal@gmail.com>
2017-11-07 19:47:22 -0800

    Comparing integration methods - 11/07/2017

--- PS2/numerical_integrator.py
+++ PS2/numerical_integrator.py
@@ -1,3 +1,4 @@
+import scipy.integrate as integrate
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -66,8 +67,6 @@ def err_plot_large(M, N):
 
     plt.xlabel('1/N')
     plt.ylabel(r'$\delta x^{1/4}$')
-    # print simp_error_list
-    # for j, e in enumerate(simp_error_list): print j, e, e**(1.0/4)
     plt.plot(1.0/n_step_list, np.abs(simp_error_list)**(1.0/4))
     plt.savefig('simp_err_%(a)s_%(b)s.png' % {"a": M, "b": N})
     plt.close()
@@ -92,11 +91,64 @@ def err_plot(N):
 
     plt.xlabel('1/N')
     plt.ylabel(r'$\delta x^{1/4}$')
-    # print simp_error_list
-    # for j, e in enumerate(simp_error_list): print j, e, e**(1.0/4)
     plt.plot(1.0/n_step_list, np.abs(simp_error_list)**(1.0/4))
     plt.savefig('simp_err_%s.png' % N)
     plt.close()
 
 
-err_plot_large(40000, 50000)
+def int_simp_specified(func, a, b, acc):
+    # Integrate externally defined function using int_simp until specified accuracy acc is reached.
+    int_current = 0
+    int_next = 0
+    k = 1
+    N_0 = 10
+    while True:
+        int_current = int_simp(func, a, b, (N_0)**k)
+        int_next = int_simp(func, a, b, (N_0)**(k + 1))
+        k += 1
+        if np.abs((int_next - int_current)/int_current) < acc:
+            return int_current
+
+
+acc = 0.01
+
+integral1 = int_simp_specified(exp, 0, 1, acc)
+expected1 = np.e - 1
+err1 = int_simp_specified(exp, 0, 1, acc) - expected1
+quad_int1 = integrate.quad(exp, 0, 1)[0]
+quad_err1 = integrate.quad(exp, 0, 1)[1]
+romberg_int1 = integrate.romberg(exp, 0, 1)
+romberg_err1 = romberg_int1 - expected1
+print('Integral: e^x from 0 to 1')
+print('Expected Value = %s' % expected1)
+print('Accuracy = %s' % acc)
+print('Value = %s' % integral1)
+print('Error = %s' % err1)
+print('scipy quad result = %s' % quad_int1)
+print('quad error = %s' % quad_err1)
+print('scipy romberg result = %s' % romberg_int1)
+print('romberg error = %s' % romberg_err1)
+
+
+def xsq(x):
+    return x**2
+
+
+integral2 = int_simp_specified(xsq, 0, 1, acc)
+expected2 = float(1./3.)
+err2 = integral2 - expected2
+quad_int2 = integrate.quad(xsq, 0, 1)[0]
+quad_err2 = integrate.quad(xsq, 0, 1)[1]
+romberg_int2 = integrate.romberg(xsq, 0, 1)
+romberg_err2 = romberg_int2 - expected2
+print('')
+print('Integral: x^2 from 0 to 1')
+print('Expected value = %s' % expected2)
+print('Accuracy = %s' % acc)
+print('Value =  %s' % integral2)
+print('Error = %s' % err2)
+print('scipy quad result = %s' % quad_int2)
+print('quad error = %s' % quad_err2)
+print('scipy romberg result = %s' % romberg_int2)
+print('romberg error = %s' % romberg_err2)
+
