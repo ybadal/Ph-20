b209e72775d13258db9e0074505086023cf047e6
Yovan Badal <yovanbadal@gmail.com>
2017-12-11 07:57:11 -0800

    Assignment 4 incl. Makefile

---
+++ PS4/config.mk
@@ -0,0 +1,6 @@
+# Main script for ODE assignment
+ODE_SOLVER=euler.py
+ODE_EXE=python $(ODE_SOLVER)
+SETTINGS= 1 0 0.01 20
+ERR_BEHAVIOR_SETTINGS= 0.01 4
+PDF_MAKER=pdflatex
--- PS4/euler.py
+++ PS4/euler.py
@@ -1,3 +1,4 @@
+import sys
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib import rcParams
@@ -29,11 +30,11 @@ def euler_plot(x_0, v_0, h, t):
     plt.xlabel('t')
     plt.ylabel('v')
     plt.plot(t_arr, v_arr)
-    plt.savefig('x_and_v_plot_%(a)s_%(b)s_%(c)s_%(d)s.png' % {"a": x_0, "b": v_0, "c": h, "d": t})
+    plt.savefig('euler_plot.png')
     plt.close()
 
 
-def euler_err(h, t):
+def euler_err(x_0, v_0, h, t):
     N = int(t/h)
     x_arr = np.zeros(N + 1)
     v_arr = np.zeros(N + 1)
@@ -65,12 +66,13 @@ def euler_err(h, t):
     plt.xlabel('t')
     plt.ylabel('$v_{analytic}(t_i) - v_i$')
     plt.plot(t_arr, v_err)
-    plt.savefig('x_err_and_v_err_plot_%(a)s_%(b)s_%(c)s_%(d)s.png' % {"a": 1, "b": 0, "c": h, "d": t})
+    plt.savefig('euler_err.png')
     plt.close()
 
 
 def maxabs(a, axis=None):
-    """Return slice of a, keeping only those values that are furthest away
+    """Return slice of a, keeping only those values that \
+            are furthest away
     from 0 along axis"""
     maxa = np.amax(a)
     mina = np.amin(a)
@@ -110,7 +112,8 @@ def max_x_err(h):
 
 
 def err_behavior(h0, k):
-    "Plotting max x error for h0/2^j; j = 0,1,2,...,k when solving up to t=20"""
+    "Plotting max x error for h0/2^j; j = 0,1,2,...,k \
+            when solving up to t=20"""
     err_array = np.zeros(k+1)
     h_array = np.zeros(k+1)
 
@@ -121,7 +124,7 @@ def err_behavior(h0, k):
     plt.xlabel('h')
     plt.ylabel('$max[x_{analytic}(t_i) - x_i]$')
     plt.plot(h_array, err_array)
-    plt.savefig('err_behavior_%(a)s_%(b)s_%(c)s.png' % {"a": h0, "b": k, "c": 20})
+    plt.savefig('err_behavior.png')
     plt.close()
 
 
@@ -145,7 +148,7 @@ def euler_energy(x_0, v_0, h, t):
     plt.xlabel('t')
     plt.ylabel('E')
     plt.plot(t_arr, energy_arr)
-    plt.savefig('energy_%(a)s_%(b)s_%(c)s_%(d)s.png' % {"a": x_0, "b": v_0, "c": h, "d": t})
+    plt.savefig('euler_energy.png')
     plt.close()
 
 
@@ -161,8 +164,10 @@ def euler_implicit_plot(x_0, v_0, h, t):
     v_arr[0] = v_0
 
     for i in range(len(t_arr) - 1):
-        x_arr[i + 1] = float(1/(h**2 + 1))*(float(x_arr[i]) + float(h)*float(v_arr[i]))
-        v_arr[i + 1] = float(1/(h**2 + 1))*(float(v_arr[i]) - float(h)*float(x_arr[i]))
+        x_arr[i + 1] = float(1/(h**2 + 1))*(float(x_arr[i]) \
+                + float(h)*float(v_arr[i]))
+        v_arr[i + 1] = float(1/(h**2 + 1))*(float(v_arr[i]) \
+                - float(h)*float(x_arr[i]))
 
     plt.figure(1)
     plt.subplot(211)
@@ -174,11 +179,11 @@ def euler_implicit_plot(x_0, v_0, h, t):
     plt.xlabel('t')
     plt.ylabel('v')
     plt.plot(t_arr, v_arr)
-    plt.savefig('x_and_v_plot_implicit_%(a)s_%(b)s_%(c)s_%(d)s.png' % {"a": x_0, "b": v_0, "c": h, "d": t})
+    plt.savefig('euler_implicit_plot.png')
     plt.close()
 
 
-def euler_implicit_err(h, t):
+def euler_implicit_err(x0, v0, h, t):
     N = int(t/h)
     x_arr = np.zeros(N + 1)
     v_arr = np.zeros(N + 1)
@@ -190,8 +195,10 @@ def euler_implicit_err(h, t):
     v_arr[0] = 0
 
     for i in range(len(t_arr) - 1):
-        x_arr[i + 1] = float(1/(h**2 + 1))*(float(x_arr[i]) + float(h)*float(v_arr[i]))
-        v_arr[i + 1] = float(1/(h**2 + 1))*(float(v_arr[i]) - float(h)*float(x_arr[i]))
+        x_arr[i + 1] = float(1/(h**2 + 1))*(float(x_arr[i]) \
+                + float(h)*float(v_arr[i]))
+        v_arr[i + 1] = float(1/(h**2 + 1))*(float(v_arr[i]) \
+                - float(h)*float(x_arr[i]))
 
     x_err = np.zeros(N + 1)
     v_err = np.zeros(N + 1)
@@ -210,7 +217,7 @@ def euler_implicit_err(h, t):
     plt.xlabel('t')
     plt.ylabel('$v_{analytic}(t_i) - v_i$')
     plt.plot(t_arr, v_err)
-    plt.savefig('implicit_x_err_and_v_err_plot_%(a)s_%(b)s_%(c)s_%(d)s.png' % {"a": 1, "b": 0, "c": h, "d": t})
+    plt.savefig('euler_implicit_err.png')
     plt.close()
 
 
@@ -228,8 +235,10 @@ def implicit_max_x_err(h):
     v_arr[0] = 0
 
     for i in range(len(t_arr) - 1):
-        x_arr[i + 1] = float(1/(h**2 + 1))*(float(x_arr[i]) + float(h)*float(v_arr[i]))
-        v_arr[i + 1] = float(1/(h**2 + 1))*(float(v_arr[i]) - float(h)*float(x_arr[i]))
+        x_arr[i + 1] = float(1/(h**2 + 1))*(float(x_arr[i]) \
+                + float(h)*float(v_arr[i]))
+        v_arr[i + 1] = float(1/(h**2 + 1))*(float(v_arr[i]) \
+                - float(h)*float(x_arr[i]))
 
     x_err = np.zeros(N + 1)
 
@@ -240,7 +249,8 @@ def implicit_max_x_err(h):
 
 
 def implicit_err_behavior(h0, k):
-    "Plotting max x error for h0/2^j; j = 0,1,2,...,k when solving up to t=20"""
+    "Plotting max x error for h0/2^j; j = 0,1,2,...,k \
+            when solving up to t=20"""
     err_array = np.zeros(k+1)
     h_array = np.zeros(k+1)
 
@@ -251,7 +261,7 @@ def implicit_err_behavior(h0, k):
     plt.xlabel('h')
     plt.ylabel('$max[x_{analytic}(t_i) - x_i]$')
     plt.plot(h_array, err_array)
-    plt.savefig('implicit_err_behavior_%(a)s_%(b)s_%(c)s.png' % {"a": h0, "b": k, "c": 20})
+    plt.savefig('implicit_err_behavior.png')
     plt.close()
 
 
@@ -267,15 +277,17 @@ def implicit_euler_energy(x_0, v_0, h, t):
     v_arr[0] = v_0
 
     for i in range(len(t_arr) - 1):
-        x_arr[i + 1] = float(1/(h**2 + 1))*(float(x_arr[i]) + float(h)*float(v_arr[i]))
-        v_arr[i + 1] = float(1/(h**2 + 1))*(float(v_arr[i]) - float(h)*float(x_arr[i]))
+        x_arr[i + 1] = float(1/(h**2 + 1))*(float(x_arr[i]) \
+                + float(h)*float(v_arr[i]))
+        v_arr[i + 1] = float(1/(h**2 + 1))*(float(v_arr[i]) \
+                - float(h)*float(x_arr[i]))
 
     energy_arr = x_arr**2 + v_arr**2
 
     plt.xlabel('t')
     plt.ylabel('E')
     plt.plot(t_arr, energy_arr)
-    plt.savefig('implicit_energy_%(a)s_%(b)s_%(c)s_%(d)s.png' % {"a": x_0, "b": v_0, "c": h, "d": t})
+    plt.savefig('implicit_euler_energy.png')
     plt.close()
 
 
@@ -295,7 +307,7 @@ def euler_phase(x_0, v_0, h, t):
     plt.ylabel('v')
     plt.plot(x_arr, v_arr)
     plt.axes().set_aspect('equal')
-    plt.savefig('euler_phase_space_%(a)s_%(b)s_%(c)s_%(d)s.png' % {"a": x_0, "b": v_0, "c": h, "d": t})
+    plt.savefig('euler_phase.png')
     plt.close()
 
 
@@ -308,14 +320,16 @@ def implicit_euler_phase(x_0, v_0, h, t):
     v_arr[0] = v_0
 
     for i in range(len(x_arr) - 1):
-        x_arr[i + 1] = float(1/(h**2 + 1))*(float(x_arr[i]) + float(h)*float(v_arr[i]))
-        v_arr[i + 1] = float(1/(h**2 + 1))*(float(v_arr[i]) - float(h)*float(x_arr[i]))
+        x_arr[i + 1] = float(1/(h**2 + 1))*(float(x_arr[i]) \
+                + float(h)*float(v_arr[i]))
+        v_arr[i + 1] = float(1/(h**2 + 1))*(float(v_arr[i]) \
+                - float(h)*float(x_arr[i]))
 
     plt.xlabel('x')
     plt.ylabel('v')
     plt.plot(x_arr, v_arr)
     plt.axes().set_aspect('equal')
-    plt.savefig('implicit_euler_phase_space_%(a)s_%(b)s_%(c)s_%(d)s.png' % {"a": x_0, "b": v_0, "c": h, "d": t})
+    plt.savefig('implicit_euler_phase.png')
     plt.close()
 
 
@@ -335,7 +349,7 @@ def symplectic_euler_phase(x_0, v_0, h, t):
     plt.ylabel('v')
     plt.plot(x_arr, v_arr)
     plt.axes().set_aspect('equal')
-    plt.savefig('symplectic_euler_phase_space_%(a)s_%(b)s_%(c)s_%(d)s.png' % {"a": x_0, "b": v_0, "c": h, "d": t})
+    plt.savefig('symplectic_euler_phase.png')
     plt.close()
 
 
@@ -352,7 +366,7 @@ def analytic_phase(x_0, v_0, h, t):
     plt.ylabel('v')
     plt.plot(x_arr, v_arr)
     plt.axes().set_aspect('equal')
-    plt.savefig('analytic_phase_space_%(a)s_%(b)s_%(c)s_%(d)s.png' % {"a": x_0, "b": v_0, "c": h, "d": t})
+    plt.savefig('analytic_phase.png')
     plt.close()
 
 
@@ -375,7 +389,19 @@ def symplectic_euler_energy(x_0, v_0, h, t):
     plt.xlabel('t')
     plt.ylabel('E')
     plt.plot(t_arr, energy_arr)
-    plt.savefig('symplectic_energy_%(a)s_%(b)s_%(c)s_%(d)s.png' % {"a": x_0, "b": v_0, "c": h, "d": t})
+    plt.savefig('symplectic_euler_energy.png')
     plt.close()
 
-err_behavior(0.01,4)
+
+def main():
+    if not (sys.argv[1] == 'err_behavior' or \
+            sys.argv[1] == 'implicit_err_behavior'):
+        eval('%(a)s(%(b)s,%(c)s,%(d)s,%(e)s)' % {"a": sys.argv[1], \
+                "b": sys.argv[2], "c": sys.argv[3], "d": sys.argv[4], \
+                "e": sys.argv[5]})
+    else:
+        eval('%(a)s(%(b)s,%(c)s)' % {"a": sys.argv[1], \
+                "b": sys.argv[2], "c": sys.argv[3]})
+
+
+main()
